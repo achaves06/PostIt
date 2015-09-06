d48c0fba8727c67d630c5130b924620e645bc761
Andrea <chaves.andrea@gmail.com>
2015-09-05 22:24:00 -0700

    Categories not pulling styles or layout

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,17 @@
+class CategoriesController < ActionController::Base
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  resources :posts, except: [:delete]    #only: [:new, :create, :edit, :update, :show]
+  resources :categories, except: [:delete]
 end
