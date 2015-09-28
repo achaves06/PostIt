cf9a1648e492ba0a42bdd2a06d7221f51d6397fe
andrea <chaves.andrea@gmail.com>
2015-09-28 16:40:55 -0700

    Added voting capability

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,4 +13,16 @@ class CommentsController < ApplicationController
       render "posts/show"
     end
 	end
+
+  def vote
+    @vote = Vote.create(vote: params[:vote], user_id: current_user.id, voteable: Comment.find(params[:id]))
+    if @vote.valid?
+      flash[:success] = "Your vote has been counted"
+      redirect_to :back
+    else
+      flash[:danger] = "Your vote was not counted, you an only vote once"
+      redirect_to :back
+    end
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def vote
+    @vote = Vote.create(vote: params[:vote], user_id: current_user.id, voteable: Post.find(params[:id]))
+    if @vote.valid?
+      flash[:success] = "Your vote has been counted"
+      redirect_to :back
+    else
+      flash[:danger] = "Your vote was not counted, you an only vote once"
+      redirect_to :back
+    end
+  end
+
   def edit
     @categories = Category.all.order('name ASC')
   end
